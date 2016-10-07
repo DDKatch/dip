e109d44e28c08fbe8ae7adb67e48372de4d33596
DDKatch <daniil.field@yandex.ru>
2016-10-07 10:47:25 +0300

    refactor made and render delay added

--- app/controllers/homeworks_controller.rb
+++ app/controllers/homeworks_controller.rb
@@ -1,51 +1,39 @@
 class HomeworksController < ApplicationController
   def index
-    case params[:operation]
-    when 'and' then
-      init_variables(:and)
-    when 'or' then
-      init_variables(:or)
-    when 'not' then
-      init_variables(:not)
-    when 'xor' then
-      init_variables(:xor)
-    else
-      init_variables(:filter)
-    end
+    @can_render = false
+    init_variables(params[:operation].to_sym)
+    @can_render = true
   end
 
   def init_variables(operation)
-    @image_name = %w{house.png const.png result.png}
+    @image_name = %w{house.png const.png result.png mask.png other.png}
     image_path = lambda{|name|Rails.root.join('app', 'assets', 'images', name)}
 
-    image = ChunkyPNG::Image.from_file(image_path.call(@image_name[0]))
-
-    @charts = []
-    @charts << image.histogram(:bright)
-
-    case operation
-    when :and || :or || :xor ||:add_c || :mult_c || :slice then
-      @image_name[1] = 'const.png'
-    when :add_p || :mult_p then
-      @image_name[1] = 'other_image.png'
-    when :mask then
-      @image_name[1] = 'mask.png'
-    when :not then
-      @image_name[1] = 'house.png'
-    else
-      temp = (ChunkyPNG::Image.new_const(image))
-      temp.save(image_path.call(@image_name[1]))
-      image.save(image_path.call(@image_name[2]))
+    images = []
+    @image_name.each_with_index do |name, i|
+      images << ChunkyPNG::Image.from_file(image_path.call(@image_name[i]))
     end
 
-    const_or_img_or_msk = ChunkyPNG::Image.from_file(image_path.call(@image_name[1]))
-    @charts << const_or_img_or_msk.histogram(:bright)
+    images[1] = ChunkyPNG::Image.new_const(images[0])
 
-    result = image.method(operation).call(const_or_img_or_msk)
+    case operation
+    when :slice then
+    when :add_p || :mult_p then
+      images[1] = images[4]
+    when :mask then
+      images[1] = ChunkyPNG::Image.new_mask(images[0])
+    when :not then
+    else
+    end
 
-    result.save(image_path.call(@image_name[2]))
-    @charts << result.histogram(:bright)
+    images[2] = images[0].method(operation).call(images[1])
 
+    @charts = []
+    3.times do |ind|
+      @charts << images[ind].histogram(:bright)
+    end
+
+    images.each_with_index {|img, ind| img.save(image_path.call(@image_name[ind]))}
   end
 
 end
